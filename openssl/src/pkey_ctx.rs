236d55d8eb2bd3d9739de3e4dda639db5b31d42c
Huw Jones <huw@pexip.com>
2025-07-31 11:41:02 +0100

    pkey_ctx: dh params generation test

--- openssl/src/pkey_ctx.rs
+++ openssl/src/pkey_ctx.rs
@@ -994,6 +994,16 @@ mod test {
 
     #[test]
     #[cfg(not(boringssl))]
+    fn dh_paramgen() {
+        let mut ctx = PkeyCtx::new_id(Id::DH).unwrap();
+        ctx.paramgen_init().unwrap();
+        ctx.set_dh_paramgen_prime_len(512).unwrap();
+        ctx.set_dh_paramgen_generator(2).unwrap();
+        ctx.paramgen().unwrap();
+    }
+
+    #[test]
+    #[cfg(not(boringssl))]
     fn dsa_paramgen() {
         let mut ctx = PkeyCtx::new_id(Id::DSA).unwrap();
         ctx.paramgen_init().unwrap();
